e12b32155a7916eb638be7169d51c9cc2d518dc8
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-12 11:36:35 -0300

    Implementação do gráfico PieChart

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import './dashboard.css';
+import React, { useState } from "react";
+import "./dashboard.css";
+import Chart from "react-google-charts";
 
 function Dashboard() {
+
+  const TITULO = "Quantidade de cadastros primeiro semestre";
+
+  const [dados, setDados] = useState([
+    ['Mês', 'Quantidade'],
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 22],
+    ['Abril', 42],
+    ['Maio', 31],
+    ['Junho', 73]
+  ])
+
   return (
-    <h1>Dashboard</h1>
+  <>
+    <h2>Dashboard</h2>
+    <Chart
+      width={'400px'}
+      height={'300px'}
+      chartType={'PieChart'}
+      data={dados}
+      options={{
+        title: TITULO
+      }} />
+    </>
   );
 }
 
--- src/dashboard.test.js
+++ src/dashboard.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Dashboard from './dashboard';
 
-test('Você deve renderizar o componente sem erros', () => {
+test('deve renderizar o componente sem erros', () => {
   render(<Dashboard />);
+  
+  // Certifique-se de que o texto 'dashboard' está presente no componente
   const linkElement = screen.getByText(/Dashboard/i);
+  
+  // Verifica se o elemento está no documento
   expect(linkElement).toBeInTheDocument();
 });
